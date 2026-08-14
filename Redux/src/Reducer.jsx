f3c9bfa2359566ea1cd4ca47b781060a5f975260
memon alfaz <memonalfaz749@gmail.com>
2026-08-14 21:59:31 +0530

    commit

--- Redux/src/Reducer.jsx
+++ Redux/src/Reducer.jsx
@@ -1,19 +1,24 @@
 import { Decrement, Increment } from "./Actiontype.jsx";
 
 const intialState = {
+
     count : 0
+
 }
 function Reducer(state = intialState,{type,payload}) // action {type,payload}
 {
     switch (type)
     {
         case Increment :
+
             return {count : state.count + payload}
  
         case Decrement :
+
             return {count : state.count - payload}    
 
         default :
+        
         return state    
     }
 }
