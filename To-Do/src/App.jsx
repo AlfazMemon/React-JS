8f6643cdeec2da2d2eeeaf0353e6c5752831e8aa
memon alfaz <memonalfaz749@gmail.com>
2026-08-23 17:22:15 +0530

    commit

--- To-Do/src/App.jsx
+++ To-Do/src/App.jsx
@@ -1,36 +1,40 @@
 import React, { useState } from 'react'
+import "./App.css"
 
 const App = () => {
 
-  const [Task,setTask] = useState("")
-  const [Tasks,setTasks] = useState([])
+  const [Task, setTask] = useState("")
+  const [Tasks, setTasks] = useState([])
 
-  function AddTask(){
-    setTasks ([...Tasks,Task])
+  function AddTask() {
+    if (Task.trim() === "") return;
+
+
+    setTasks([...Tasks, Task])
     setTask("")
   }
 
-  function DeleteTask(index){
-    const NewTask = Tasks.filter((_,i)=> i !== index)
+  function DeleteTask(index) {
+    const NewTask = Tasks.filter((_, i) => i !== index)
     setTasks(NewTask)
   }
 
   return (
     <div>
-        <input type="text" placeholder='Enter Task Name' value={Task} onChange={(e)=>setTask(e.target.value)}/>
-        <button onClick={AddTask}>Add Task</button>
+      <input type="text" placeholder='Enter Task Name' value={Task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={AddTask}>Add Task</button>
 
       <ul>
         {
-          Tasks.map((Task,index)=>(
-           
+          Tasks.map((Task, index) => (
+
             <li key={index}>
-              {Task} 
-              <button onClick={()=> DeleteTask(index)}>Delete</button>
-            </li> 
+              {Task}
+              <button onClick={() => DeleteTask(index)}>Delete</button>
+            </li>
           ))
         }
-      </ul>   
+      </ul>
 
     </div>
   )
