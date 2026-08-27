1dde5870e03d17a3f6a9c4025929b7b2de0dcb5a
memon alfaz <memonalfaz749@gmail.com>
2026-08-27 19:52:46 +0530

    commit

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import Home from './pages/Home'
-import Pnf from './pages/Pnf'
 import About from './pages/About'
 import Blog from './pages/Blog'
+import Pnf from './Pnf'
 import Contact from './pages/Contact'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './Navbar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/About" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Pnf />} />
-
       </Routes>
     </div>
   )
---
+++ router/src/Pnf.jsx
@@ -0,0 +1,12 @@
+const Pnf = () =>{
+
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+
+
+}
+
+export default Pnf
